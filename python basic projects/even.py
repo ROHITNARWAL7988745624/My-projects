ebd3a98ac53819cb6575e73bb7b9877b4d5b9098
ROHITNARWAL7988745624 <79442284+ROHITNARWAL7988745624@users.noreply.github.com>
2021-02-23 11:12:40 +0530

    Add files via upload

---
+++ python basic projects/even.py
@@ -0,0 +1,6 @@
+# To check wheather a number is even or odd
+num =int(input( "Enter a integer :"))
+if num%2 == 0 :
+    print(num, " is a even number.")
+else :
+    print(num, " is a odd number.")
